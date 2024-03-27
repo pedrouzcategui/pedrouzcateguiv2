dd15ef97f27ae4d36ab7fb38179c600525326a0d
Pedro Uzcategui <pedrouzcategui0103@gmail.com>
2024-03-26 20:36:53 -0400

    Add MDX and changes article

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -15,38 +15,42 @@ export default function Menu() {
     }
 
     return (
-        <nav className="z-50 fixed shadow-sm top-0 left-0 w-full py-4 bg-white/30 backdrop-filter backdrop-blur-lg dark:bg-gray-900/30">
-            <div className="mx-auto w-4/5 font-bold flex justify-between ">
-                <div>
-                    <Link href={'/'}>
-                        <span>@rowancode</span>
-                    </Link>
+        <>
+            <nav className="z-50 fixed shadow-sm top-0 left-0 w-full py-4 bg-white/30 backdrop-filter backdrop-blur-lg dark:bg-gray-900/30">
+                <div className="mx-auto w-4/5 font-bold flex justify-between ">
+                    <div>
+                        <Link href={'/'}>
+                            <span>@rowancode</span>
+                        </Link>
+                    </div>
+                    <ul className="flex gap-4">
+                        <li>
+                            <Link href={'mailto:contact@rowandev.com'}>
+                                <FaEnvelope size={24} />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={'https://www.linkedin.com/in/pedro-uzcategui/'}>
+                                <FaLinkedin size={24} />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={'https://github.com/pedrouzcategui'}>
+                                <FaGithub size={24} />
+                            </Link>
+                        </li>
+                        <li className="cursor-pointer">
+                            {
+                                isDark
+                                    ? <FaSun size={24} onClick={handleDarkToggle} />
+                                    : <FaMoon size={24} onClick={handleDarkToggle} />
+                            }
+                        </li>
+                    </ul>
                 </div>
-                <ul className="flex gap-4">
-                    <li>
-                        <Link href={'mailto:contact@rowandev.com'}>
-                            <FaEnvelope size={24} />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href={'https://www.linkedin.com/in/pedro-uzcategui/'}>
-                            <FaLinkedin size={24} />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href={'https://github.com/pedrouzcategui'}>
-                            <FaGithub size={24} />
-                        </Link>
-                    </li>
-                    <li className="cursor-pointer">
-                        {
-                            isDark
-                                ? <FaSun size={24} onClick={handleDarkToggle} />
-                                : <FaMoon size={24} onClick={handleDarkToggle} />
-                        }
-                    </li>
-                </ul>
-            </div>
-        </nav>
+            </nav>
+            <div className="w-full py-8"></div>
+        </>
+
     )
 }
